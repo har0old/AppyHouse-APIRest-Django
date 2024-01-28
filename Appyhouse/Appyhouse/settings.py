d56269192894c67a8917c0270d888da859e01e04
Harold Garcia <garciavargas9112@gmail.com>
2024-01-28 15:01:30 -0500

    Integrando Postgres

--- Appyhouse/Appyhouse/settings.py
+++ Appyhouse/Appyhouse/settings.py
@@ -81,13 +81,22 @@ AUTH_USER_MODEL = 'user_app.Account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'postgres',
+        'PASSWORD': '******',
+        'HOST': 'localhost',
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -146,6 +155,9 @@ REST_FRAMEWORK = {
         'comment-create':'20/day',
         'commet-detail':'30/day',
     },
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     #'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
     #'PAGE_SIZE':10,
 }
